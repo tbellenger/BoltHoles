c5d976d3a54d3062733a37c66f1c68764ad47f1f
tbellenger <tbellenger@gmail.com>
2021-09-17 17:45:54 -0700

    changed to generate svg instead of draw on canvas

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ let tick = 0;
 // get the canvas
 let c = document.getElementById("myChart");
 let ctx = c.getContext("2d");
+var svg = d3.select("svg");
 // calculate extents and midpoint
 let canvY = c.height;
 let canvX = c.width;
@@ -13,8 +14,8 @@ let canvMidY = canvY / 2;
 
 // get page search params for hole circle params
 document.getElementById("graph").onclick = function () {
-    ctx.beginPath();
-    ctx.clearRect(0, 0, c.width, c.height);
+    // remove any graphics currently in the svg
+    svg.selectAll("*").remove();
     r = document.getElementById("r").value;
     h = document.getElementById("h").value;
     s = document.getElementById("s").value;
@@ -48,13 +49,24 @@ function holes(radius, numHoles, startAngle) {
 }
 
 function draw(d) {
-    // draw x and y axis
-    ctx.moveTo(canvMidX, 0);
-    ctx.lineTo(canvMidX, canvY);
-    ctx.stroke();
-    ctx.moveTo(0, canvMidY);
-    ctx.lineTo(canvX, canvMidY);
-    ctx.stroke();
+    // draw x axis in svg
+    svg.append("line")
+    .attr("class", "x-axis")
+    .attr("x1", canvMidX)
+    .attr("y1", 0)
+    .attr("x2", canvMidX)
+    .attr("y2", canvY)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+    // draw y axis in svg
+    svg.append("line")
+    .attr("class", "y-axis")
+    .attr("x1", 0)
+    .attr("y1", canvMidY)
+    .attr("x2", canvX)
+    .attr("y2", canvMidY)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
 
     // calculate tick size according to canvas size
     tick = Math.min(canvX, canvY) / ((2 * r) + 1);
@@ -62,11 +74,15 @@ function draw(d) {
     // draw holes and point text
     d.forEach(drawPoint);
 
-    // draw bolt hole circle
-    ctx.beginPath()
-    ctx.arc(canvMidX, canvMidY, tick * r, 0, 2 * Math.PI);
-    ctx.closePath();
-    ctx.stroke();
+    // draw bolt hole circle in svg
+    svg.append("circle")
+    .attr("class", "hole-circle")
+    .attr("cx", canvMidX)
+    .attr("cy", canvMidY)
+    .attr("r", tick * r)
+    .attr("fill", "none")
+    .attr("stroke", "black");
+
     // draw the ticks on the axes
     drawTicks(r, tick, tick);
 }
@@ -75,12 +91,19 @@ function draw(d) {
 function drawPoint(item, index) {
     let circX = (canvMidX + (tick * parseFloat(item.x)));
     let circY = (canvMidY - (tick * parseFloat(item.y)));
-    ctx.beginPath();
-    ctx.arc(circX, circY, 5, 0, 2 * Math.PI);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.font = "11px Arial";
-    ctx.strokeText(item.x + ", " + item.y, circX-40, circY+20);
+
+    svg.append("circle")
+    .attr("class", "hole-point")
+    .attr("cx", circX)
+    .attr("cy", circY)
+    .attr("r", 5)
+    .attr("fill", "none")
+    .attr("stroke", "black");
+    svg.append("text")
+    .attr("class", "hole-text")
+    .attr("x", circX - 40)
+    .attr("y", circY + 20)
+    .text(item.x + ", " + item.y);
 }
 
 // draw the ticks on the axes
@@ -96,19 +119,39 @@ function drawTicks(rad, tx, ty) {
             tkSz = 4;
         }
         // above and below the x axis on both negative and positive sides
-        ctx.moveTo(canvMidX + counter * tx, canvMidY - tkSz);
-        ctx.lineTo(canvMidX + counter * tx, canvMidY + tkSz);
-        ctx.stroke();
-        ctx.moveTo(canvMidX - counter * tx, canvMidY - tkSz);
-        ctx.lineTo(canvMidX - counter * tx, canvMidY + tkSz);
-        ctx.stroke();
+        svg.append("line")
+        .attr("class", "tick")
+        .attr("x1", canvMidX + counter * tx)
+        .attr("y1", canvMidY - tkSz)
+        .attr("x2", canvMidX + counter * tx)
+        .attr("y2", canvMidY + tkSz)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+        svg.append("line")
+        .attr("class", "tick")
+        .attr("x1", canvMidX - counter * tx)
+        .attr("y1", canvMidY - tkSz)
+        .attr("x2", canvMidX - counter * tx)
+        .attr("y2", canvMidY + tkSz)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
         // to the left and right of the y axis on both negative and positive sides
-        ctx.moveTo(canvMidX - tkSz, canvMidY + counter * ty);
-        ctx.lineTo(canvMidX + tkSz, canvMidY + counter * ty);
-        ctx.stroke();
-        ctx.moveTo(canvMidX - tkSz, canvMidY - counter * ty);
-        ctx.lineTo(canvMidX + tkSz, canvMidY - counter * ty);
-        ctx.stroke();
+        svg.append("line")
+        .attr("class", "tick")
+        .attr("x1", canvMidX - tkSz)
+        .attr("y1", canvMidY + counter * ty)
+        .attr("x2", canvMidX + tkSz)
+        .attr("y2", canvMidY + counter * ty)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
+        svg.append("line")
+        .attr("class", "tick")
+        .attr("x1", canvMidX - tkSz)
+        .attr("y1", canvMidY - counter * ty)
+        .attr("x2", canvMidX + tkSz)
+        .attr("y2", canvMidY - counter * ty)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1);
         // work in .5 increments for the ticks
         counter = counter + 0.5;
 
